9d5384e7de907a77071dca98da544a4e5128a424
astsiaresh <coffee.injoy@gmail.com>
2020-07-26 23:25:49 +0300

    change config log

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     file_name = 'logs/log_'+str(datetime.now().strftime("%Y-%m-%d"))+'.log'
     file_log = logging.FileHandler(file_name)
     console_out = logging.StreamHandler()
-    logging.basicConfig(handlers=(file_log, console_out), level=logging.INFO)
+    format_stdout = u'%(levelname)-8s [%(asctime)s]: %(message)s'
+    logging.basicConfig(handlers=(file_log, console_out), level=logging.INFO, format=format_stdout, datefmt='%d-%b-%y')
     db.create_all()
     app.run()
